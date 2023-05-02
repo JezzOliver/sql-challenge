0579281c4fecce39f87ac75815a2f6237ded5dc1
JezzOliver <shangajism@hotmail.com>
2023-05-02 23:42:51 +1000

    Queries added

--- EmployeeSQL/TableSchema.sql
+++ EmployeeSQL/TableSchema.sql
@@ -19,6 +19,7 @@ CREATE TABLE dept_emp (
 	emp_no	INT NOT NULL,
 	dept_no	VARCHAR(4) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 SELECT * FROM dept_emp
@@ -48,7 +49,7 @@ CREATE TABLE dept_manager (
 	dept_no	VARCHAR(4) NOT NULL,
 	emp_no	INT NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	
 );
 
 SELECT * FROM dept_manager
